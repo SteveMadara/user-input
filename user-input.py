0f0c631c9314626fbf9ff1be803f2893bab9f1cd
SteveMadara <stephen.madara@gmail.com>
2024-08-22 22:28:28 +0300

    Add files via upload

--- user-input.py
+++ user-input.py
@@ -1,1 +1,4 @@
-user-input
+name= input("What is your name: ")
+age= input("What is your age:")
+location= ("What is your location:")
+print("Hello" [name], "you are" [age], "and you live in" [location])
